e7af44c9fb6ffb0b873eb5a0db551d2e5b70e1e0
stas korchevskyi <stas_1999_nr@ukr.net>
2023-05-01 16:52:01 +0300

    Добавьте файлы проекта.

---
+++ Recomendation.Api/Controllers/RecomendationsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Recomendation.Api.Controllers
+{
+    [Route("api/recomendations")]
+    [ApiController]
+    public class RecomendationsController : ControllerBase
+    {
+    }
+}
